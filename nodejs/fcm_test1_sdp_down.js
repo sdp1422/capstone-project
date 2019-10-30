b9e184601a3bc95bf9d51f319009a8b28d59fb67
knut-sdpark <sdp14222@gmail.com>
2019-10-31 03:31:18 +0900

    modified 'client_token & message 내용' - '내 데이터'

--- nodejs/fcm_test1_sdp_down.js
+++ nodejs/fcm_test1_sdp_down.js
@@ -2,14 +2,16 @@ var FCM = require('fcm-node');
 
 var serverKey = 'AAAAyW2dkpk:APA91bHWFYE4xIDX9JOWix6SKukgeH-AWfTrBe9b3G-XNO-9V0uNc-L6ngEQ929HEFB1r1G_KBbagdVSR895cMOg3KErvJR_jG7yQvVu5w1rr310u6ynyi_dg7CSYNtpgrh82q5hJhin';
 
-var client_token = 'dLjdqzaPDzE:APA91bFTDWKpFiAPIYEbA_zYNwMefp46o5rEDSbNnxpgI1A0oTsGMrKllqeGzx47Eq4upZhYz5GoQIbljYLJmj4GIrJ5WFGDED5_ns3IAQus1abIw5wYCVqeFbS1aq_tmP2xD7KCXtz-';
+//var client_token = 'dLjdqzaPDzE:APA91bFTDWKpFiAPIYEbA_zYNwMefp46o5rEDSbNnxpgI1A0oTsGMrKllqeGzx47Eq4upZhYz5GoQIbljYLJmj4GIrJ5WFGDED5_ns3IAQus1abIw5wYCVqeFbS1aq_tmP2xD7KCXtz-';
+
+var client_token = 'ed6KBZnxqgY:APA91bFyQx-hs009TJcAK8m5QkrNhTr7mp6uqImKRk0dWVgk9eEp3oX1RF-h7iUkWgdrMXZjoWUMaTve6psjQ_dNw7iNHRd3KHuiSKAMEl0P70ILodIfIj6If4cZtAL1uFrq0Auf8xUU';
 
 var push_data = {
 	to: client_token,
 
 	notification: {
 		title: "유치원통학버스봇",
-		body: "박상돈님이 하차하였습니다. 박상돈님 부모님께 발송하는 메세지입니다.",
+		body: "박상돈님이 하차하였습니다.",
 		sound: "default",
 		click_action: "FCM_PLUGIN_ACTIVITY",
 		icon: "fcm_push_icon"
@@ -29,11 +31,11 @@ var fcm = new FCM(serverKey);
 
 fcm.send(push_data, function(err, response) {
 	if(err) {
-		console.error('Push메시지 발송에 실패했습니다.');
+		console.error('Push message error - ParkSangDon down.');
 		console.error(err);
 		return;
 	}
 
-	console.log('Push메시지가 발송되었습니다.');
+	console.log('Push Success : ParkSangDon down.');
 	console.log(response);
 });
--- nodejs/fcm_test1_sdp_up.js
+++ nodejs/fcm_test1_sdp_up.js
@@ -2,14 +2,16 @@ var FCM = require('fcm-node');
 
 var serverKey = 'AAAAyW2dkpk:APA91bHWFYE4xIDX9JOWix6SKukgeH-AWfTrBe9b3G-XNO-9V0uNc-L6ngEQ929HEFB1r1G_KBbagdVSR895cMOg3KErvJR_jG7yQvVu5w1rr310u6ynyi_dg7CSYNtpgrh82q5hJhin';
 
-var client_token = 'e32Gvxx090M:APA91bGA8LLICfvLdbRP-fvm7ga5Yb7Sv8Xia0kp_ocjLCaldu5v1prSr5FlTqyC722QCWFJK4YNDIvsNAPSViWYBjdaEPcN8PpUaEojPKEeCXOlqKC3GojhoD3Mcl8gMCemj5_X8wWq';
+//var client_token = 'e32Gvxx090M:APA91bGA8LLICfvLdbRP-fvm7ga5Yb7Sv8Xia0kp_ocjLCaldu5v1prSr5FlTqyC722QCWFJK4YNDIvsNAPSViWYBjdaEPcN8PpUaEojPKEeCXOlqKC3GojhoD3Mcl8gMCemj5_X8wWq';
+
+var client_token = 'ed6KBZnxqgY:APA91bFyQx-hs009TJcAK8m5QkrNhTr7mp6uqImKRk0dWVgk9eEp3oX1RF-h7iUkWgdrMXZjoWUMaTve6psjQ_dNw7iNHRd3KHuiSKAMEl0P70ILodIfIj6If4cZtAL1uFrq0Auf8xUU';
 
 var push_data = {
 	to: client_token,
 
 	notification: {
-		title: "title - sdp_up",
-		body: "body - sdp_up.",
+		title: "유치원통학버스봇",
+		body: "박상돈님이 승차하였습니다.",
 		sound: "default",
 		click_action: "FCM_PLUGIN_ACTIVITY",
 		icon: "fcm_push_icon"
@@ -29,11 +31,11 @@ var fcm = new FCM(serverKey);
 
 fcm.send(push_data, function(err, response) {
 	if(err) {
-		console.error('Push Message Failed.');
+		console.error('Push message error - ParkSangDon up.');
 		console.error(err);
 		return;
 	}
 
-	console.log('Push Message is sent.');
+	console.log('Push Success : ParkSangDon up.');
 	console.log(response);
 });
